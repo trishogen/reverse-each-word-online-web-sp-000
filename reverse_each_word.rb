3a67b367219f20ecfffee588713df59dce84be67
Trish Ogen <patriciaogen@gmail.com>
2019-05-17 01:49:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_string = ""
   array.each do |word|
-    new_string << word.reverse!
+    new_string << "#{word.reverse!} "
   end
   new_string
 end
